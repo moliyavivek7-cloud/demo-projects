6afa8f060eeea50e48ed209fda9be8bd6ce9a890
moliyavivek7-cloud <moliyavivek7@gmail.com>
2026-01-06 12:10:25 +0530

    Update myperfume.php

--- myperfume.php
+++ myperfume.php
@@ -7,6 +7,6 @@ $db   = "your_database_name";
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: ". $conn->connect_error);
 } 
 ?>
