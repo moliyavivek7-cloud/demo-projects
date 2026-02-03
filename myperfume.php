52cdc05e5110a5d1e95ec1e39d0766bd6531d921
moliyavivek7-cloud <moliyavivek7@gmail.com>
2026-02-03 11:59:08 +0530

    Update myperfume.php

--- myperfume.php
+++ myperfume.php
@@ -4,7 +4,6 @@ $user = "root"; // Default XAMPP username
 $pass = "";     // Default XAMPP password
 $db   = "your_database_name";
 $conn = new mysqli($host, $user, $pass, $db);
-
 if ($conn->connect_error) {
     die("Connection failed: ". $conn->connect_error);
     
